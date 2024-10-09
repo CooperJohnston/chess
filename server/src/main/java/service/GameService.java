eeb116d57b9d624f3b6751ce74b47995da0eaa68
CooperJohnston <coopera1@byu.edu>
2024-10-09 14:49:45 -0600

    added template for the service

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,11 @@
+package service;
+
+import model.GameData;
+
+import java.util.ArrayList;
+
+public class GameService {
+  GameData joinGame(JoinGameRequest){}
+  ArrayList<GameData> ListGames(auhtToken){}
+  GameData CreateGame(authtoken){}
+}
