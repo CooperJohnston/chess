b242de6f4358781e4fe77e8ff3f6643d3fbe46fd
CooperJohnston <coopera1@byu.edu>
2024-10-14 02:01:18 -0600

    updated GAME DAO; now to implement the memory access

--- server/src/main/java/dataaccess/GameDAO.java
+++ server/src/main/java/dataaccess/GameDAO.java
@@ -1,5 +1,12 @@
 package dataaccess;
 
-public interface GameDAO {
+import model.GameData;
 
+import java.util.ArrayList;
+
+public interface GameDAO {
+  String createGame(GameData game);
+  GameData getGame(int id);
+  void updateGame(GameData game);
+  ArrayList<GameData> getAllGames();
 }
