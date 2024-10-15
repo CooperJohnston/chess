7edd23d337a71aa57e2e0e6908c476c749aeab36
CooperJohnston <coopera1@byu.edu>
2024-10-15 08:47:15 -0600

    Auth data now collected

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -3,10 +3,14 @@ package dataaccess;
 import model.AuthData;
 
 public interface AuthDAO {
-  void creatAuthData(String Username) throws DataAccessException;
+  void createAuthData(AuthData NewAuth) throws DataAccessException;
+
   boolean checkAuthData(String AutToken) throws DataAccessException;
+
   AuthData getAuthData(String AutToken) throws DataAccessException;
+
   void deleteAuthData(String AutToken) throws DataAccessException;
+
   void clear() throws DataAccessException;
 
 }
