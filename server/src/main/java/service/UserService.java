778b52b2d271e275e0efd7938b017f6de36f0f88
CooperJohnston <coopera1@byu.edu>
2024-10-17 12:13:47 -0600

    added login request and and repsonse.

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -4,6 +4,10 @@ import dataaccess.DataAccessException;
 import dataaccess.UserDAO;
 import model.AuthData;
 import model.UserData;
+import requests.LoginRequest;
+import requests.RegisterRequest;
+import responses.LoginResponse;
+import responses.RegisterResponse;
 
 public class UserService {
   private final UserDAO DAO;
@@ -15,4 +19,32 @@ public class UserService {
   public void clear() throws DataAccessException {
     this.DAO.clear();
   }
+
+  public RegisterResponse registerUser(RegisterRequest regRequest) throws DataAccessException {
+    if ((regRequest.getPassword() == null) || (regRequest.getEmail() == null) || (regRequest.getUsername() == null)) {
+      throw new DataAccessException("Username and password are required");
+    }
+    UserData userData=new UserData(regRequest.getUsername(), regRequest.getPassword(), regRequest.getEmail());
+    if (DAO.checkUser(userData)) {
+      throw new DataAccessException("User already exists in the database");
+    }
+
+    DAO.insertUser(userData);
+    return new RegisterResponse(null, regRequest.getUsername());
+
+  }
+
+  public LoginResponse loginUser(LoginRequest loginRequest) throws DataAccessException {
+    if (loginRequest.getUsername() == null || loginRequest.getPassword() == null) {
+      throw new DataAccessException("Username and password are required");
+    }
+    UserData userData=new UserData(loginRequest.getUsername(), loginRequest.getPassword(), null);
+    if (!DAO.checkUser(userData) || !DAO.getUser(userData).password().equals(loginRequest.getPassword())) {
+      throw new DataAccessException("User is not recognized");
+    }
+    return new LoginResponse(loginRequest.getUsername(), null);
+
+  }
+
+
 }
