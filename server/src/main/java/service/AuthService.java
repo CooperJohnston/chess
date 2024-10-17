69dcd974fb2850d67e3d09895f265d3e4ea52ca3
CooperJohnston <coopera1@byu.edu>
2024-10-17 12:45:04 -0600

    added login request and and repsonse.

--- server/src/main/java/service/AuthService.java
+++ server/src/main/java/service/AuthService.java
@@ -1,8 +1,12 @@
 package service;
 
+import java.util.UUID;
+
 import dataaccess.AuthDAO;
 import dataaccess.DataAccessException;
 import model.AuthData;
+import requests.LoginRequest;
+import requests.RegisterRequest;
 
 public class AuthService {
 
@@ -15,5 +19,33 @@ public class AuthService {
   void clear() throws DataAccessException {
     DAO.clear();
   }
+
+  public String makeAuth(String username) throws DataAccessException {
+    String token=UUID.randomUUID().toString();
+    AuthData authData=new AuthData(username, token);
+    DAO.createAuthData(authData);
+    return token;
+  }
+
+  public String makeAuth(RegisterRequest registerRequest) throws DataAccessException {
+    return makeAuth(registerRequest.getUsername());
+  }
+
+  public String makeAuth(LoginRequest loginRequest) throws DataAccessException {
+    return makeAuth(loginRequest.getUsername());
+  }
+
+  public void logout(String token) throws DataAccessException {
+    DAO.deleteAuthData(token);
+  }
+
+  public boolean authenticate(String token) throws DataAccessException {
+    if (DAO.checkAuthData(token)) {
+      return true;
+    } else {
+      throw new DataAccessException("User is unauthorized");
+    }
+  }
+
 
 }
