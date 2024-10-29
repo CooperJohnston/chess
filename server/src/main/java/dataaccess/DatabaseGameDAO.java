f5225fee9a70f3acc0effe068d298218bd21c15d
CooperJohnston <coopera1@byu.edu>
2024-10-29 00:20:30 -0600

    gameDAO

--- server/src/main/java/dataaccess/DatabaseGameDAO.java
+++ server/src/main/java/dataaccess/DatabaseGameDAO.java
@@ -1,4 +1,152 @@
 package dataaccess;
 
-public class DatabaseGameDAO {
+import chess.ChessGame;
+import com.google.gson.Gson;
+import model.GameData;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class DatabaseGameDAO implements GameDAO {
+  public DatabaseGameDAO() throws DataAccessException {
+    DatabaseManager.fillTables();
+
+  }
+
+  @Override
+  public void createGame(GameData game) throws DataAccessException {
+    try (Connection connection=DatabaseManager.getConnection()) {
+      String query="INSERT INTO GameData (gameID, whiteUsername, blackUsername, " +
+              "gameName, game) VALUES (?, ?, ?, ?, ?)";
+      try (PreparedStatement statement=connection.prepareStatement(query)) {
+        statement.setInt(1, game.gameID());
+        statement.setString(2, game.whiteUsername());
+        statement.setString(3, game.blackUsername());
+        statement.setString(4, game.gameName());
+        ChessGame chessGame=game.game();
+        Gson gson=new Gson();
+        String json=gson.toJson(chessGame);
+        statement.setString(5, json);
+        statement.executeUpdate();
+      }
+    } catch (SQLException | DataAccessException e) {
+      throw new DataAccessException(e.getMessage());
+    }
+  }
+
+  @Override
+  public GameData getGame(int id) throws DataAccessException {
+    try (Connection connection=DatabaseManager.getConnection()) {
+      String query="SELECT * FROM GameData WHERE gameID = ?";
+      try (PreparedStatement preparedStatement=connection.prepareStatement(query)) {
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet=preparedStatement.executeQuery();
+        if (resultSet.next()) {
+          var game=resultSet.getString("game");
+          Gson gson=new Gson();
+          ChessGame chessGame=gson.fromJson(resultSet.getString("game"), ChessGame.class);
+          return new GameData(id, resultSet.getString("whiteUsername"), resultSet.getString("blackUsername"),
+                  resultSet.getString("gameName"), chessGame);
+        }
+      }
+    } catch (SQLException | DataAccessException e) {
+      throw new DataAccessException(e.getMessage());
+    }
+    return null;
+  }
+
+  @Override
+  public boolean checkGame(int gameID) throws DataAccessException {
+    try (Connection connection=DatabaseManager.getConnection()) {
+      String query="SELECT * FROM GameData WHERE gameID = ?";
+      try (PreparedStatement preparedStatement=connection.prepareStatement(query)) {
+        preparedStatement.setInt(1, gameID);
+        ResultSet resultSet=preparedStatement.executeQuery();
+        if (resultSet.next()) {
+          return resultSet.getInt("count") > 0;
+        }
+      }
+    } catch (SQLException | DataAccessException e) {
+      throw new DataAccessException(e.getMessage());
+    }
+    return false;
+  }
+
+  @Override
+  public boolean checkGame(String gameName) throws DataAccessException {
+    try (Connection connection=DatabaseManager.getConnection()) {
+      String query="SELECT * FROM GameData WHERE gameName = ?";
+      try (PreparedStatement preparedStatement=connection.prepareStatement(query)) {
+        preparedStatement.setString(1, gameName);
+        ResultSet resultSet=preparedStatement.executeQuery();
+        if (resultSet.next()) {
+          return resultSet.getInt("count") > 0;
+        }
+      }
+    } catch (SQLException | DataAccessException e) {
+      throw new DataAccessException(e.getMessage());
+    }
+    return false;
+  }
+
+  @Override
+  public void updateGame(GameData game) throws DataAccessException {
+    String query="UPDATE GameData SET whiteUsername = ?, blackUsername = ? WHERE gameID = ?";
+    if (getGame(game.gameID()) == null) {
+      throw new DataAccessException("Game does not exist");
+    }
+
+    String whiteUsername=game.whiteUsername();
+    String blackUsername=game.blackUsername();
+    try (Connection connection=DatabaseManager.getConnection()) {
+      PreparedStatement preparedStatement=connection.prepareStatement(query);
+      preparedStatement.setString(1, whiteUsername);
+      preparedStatement.setString(2, blackUsername);
+      preparedStatement.setInt(3, game.gameID());
+      preparedStatement.executeUpdate();
+    } catch (SQLException | DataAccessException e) {
+      throw new DataAccessException(e.getMessage());
+    }
+
+  }
+
+  @Override
+  public ArrayList<GameData> getAllGames() throws DataAccessException {
+    ArrayList<GameData> games=new ArrayList<>();
+    try (Connection connection=DatabaseManager.getConnection()) {
+      String query="SELECT * FROM GameData";
+      try (PreparedStatement preparedStatement=connection.prepareStatement(query)) {
+        ResultSet resultSet=preparedStatement.executeQuery();
+        while (resultSet.next()) {
+          var game=resultSet.getString("game");
+          Gson gson=new Gson();
+          ChessGame chessGame=gson.fromJson(resultSet.getString("game"), ChessGame.class);
+          GameData gameData=new GameData(resultSet.getInt("gameID"), resultSet.getString("whiteUsername"),
+                  resultSet.getString("blackUsername"), resultSet.getString("gameName"), chessGame);
+          games.add(gameData);
+        }
+      }
+
+    } catch (SQLException | DataAccessException e) {
+      throw new DataAccessException(e.getMessage());
+    }
+    return games;
+  }
+
+  @Override
+  public void clear() throws DataAccessException {
+    try (Connection connection=DatabaseManager.getConnection()) {
+      String query="DELETE FROM GameData";
+      try (PreparedStatement preparedStatement=connection.prepareStatement(query)) {
+        preparedStatement.executeUpdate();
+
+      }
+
+    } catch (SQLException | DataAccessException e) {
+      throw new DataAccessException(e.getMessage());
+    }
+  }
 }
