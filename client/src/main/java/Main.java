a163233b379c82794d927be60ac49195a9720367
CooperJohnston <coopera1@byu.edu>
2024-11-04 14:58:33 -0700

    more frameworks for phase 5

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,15 @@
 import chess.*;
+import ui.Repl;
 
 public class Main {
-    public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+  public static void main(String[] args) {
+    var piece=new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+    System.out.println("♕ 240 Chess Client: " + piece);
+    var serverURL="http://localhost:8080";
+    if (args.length > 0) {
+      serverURL=args[0];
     }
+    var repl=new Repl(serverURL);
+    repl.run();
+  }
 }
