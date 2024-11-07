9b8bd5240c447049951bfd962d76d6173d82c6e6
CooperJohnston <coopera1@byu.edu>
2024-11-06 17:29:12 -0700

    worked on Repl

--- client/src/main/java/ui/Repl.java
+++ client/src/main/java/ui/Repl.java
@@ -1,4 +1,38 @@
 package ui;
 
+import java.util.Scanner;
+
 public class Repl {
+  private final Client client;
+
+  public Repl(String url) {
+    client=new Client(url);
+  }
+
+  public void run() {
+    System.out.println("Welcome to Chess.");
+    System.out.print(client.help());
+
+    Scanner scanner=new Scanner(System.in);
+    var result="";
+    while (!result.equals("quit")) {
+      printPrompt();
+      String line=scanner.nextLine();
+
+      try {
+        result=client.eval(line);
+        System.out.print(result);
+      } catch (Throwable e) {
+        var msg=e.toString();
+        System.out.print(msg);
+      }
+    }
+    System.out.println();
+  }
+
+
+  private void printPrompt() {
+    System.out.print("\n" + ">>> ");
+  }
+
 }
