8420a5da093a6dd79115b37a3b20e1ed477b05de
CooperJohnston <coopera1@byu.edu>
2024-11-26 12:41:34 -0700

    connect message ;)

--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -9,8 +9,11 @@ import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 import service.AuthService;
 import service.GameService;
 import service.UserService;
+import websocket.commands.JoinGameCommand;
 import websocket.commands.UserGameCommand;
 import websocket.messages.ErrorMessage;
+import websocket.messages.LoadGameMessage;
+import websocket.messages.Notification;
 import websocket.messages.ServerMessage;
 
 import java.io.IOException;
@@ -56,6 +59,34 @@ public class WebSocketHandler {
 
   }
 
+  public void connect(String message, Session session) throws IOException {
+    try {
+      JoinGameCommand command=new Gson().fromJson(message, JoinGameCommand.class);
+      connectionManager.add(command.getAuthToken(), session, command.getGameID());
+      String username=authService.getAuthData(command.getAuthToken()).username();
+      ChessGame chessGame=getGame(command.getGameID()).game();
+      if (command.getColor() == null) {
+        String messageReturn=String.format("%s has joined your game", username);
+        var obs=new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, chessGame);
+        connectionManager.sendMessage(command.getAuthToken(), obs, command.getGameID());
+
+        var notification=new Notification(ServerMessage.ServerMessageType.NOTIFICATION, messageReturn);
+        connectionManager.broadcast(command.getAuthToken(), notification, command.getGameID());
+
+      } else {
+        var obs=new LoadGameMessage(ServerMessage.ServerMessageType.LOAD_GAME, chessGame);
+        connectionManager.sendMessage(command.getAuthToken(), obs, command.getGameID());
+        String messageReturn=String.format("%s has joined your game as %s", username, command.getColor());
+        var notification=new Notification(ServerMessage.ServerMessageType.NOTIFICATION, messageReturn);
+        connectionManager.broadcast(command.getAuthToken(), notification, command.getGameID());
+
+      }
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+
   public void resign(String authToken, int gameID, UserGameCommand command) throws IOException {
     try {
       String userOne=authService.getAuthData(authToken).username();
@@ -75,6 +106,22 @@ public class WebSocketHandler {
         return;
       }
 
+      String victor;
+      if (turn == ChessGame.TeamColor.WHITE) {
+        victor=currData.blackUsername();
+        chessGame.resign(ChessGame.TeamColor.WHITE);
+        GameData winnerData=new GameData(currData.gameID(), userTwo, userOne, currData.gameName(), chessGame);
+        gameService.addWinner(winnerData);
+      } else {
+        victor=currData.whiteUsername();
+        chessGame.resign(ChessGame.TeamColor.BLACK);
+        GameData winnerData=new GameData(currData.gameID(), userTwo, userOne, currData.gameName(), chessGame);
+        gameService.addWinner(winnerData);
+
+      }
+      Notification winMess=new Notification(ServerMessage.ServerMessageType.NOTIFICATION, String.format("%s has resigned", victor));
+      connectionManager.broadcast(authToken, winMess, command.getGameID());
+      connectionManager.sendMessage(authToken, winMess, command.getGameID());
 
     } catch (Exception e) {
       ErrorMessage error=new ErrorMessage(ServerMessage.ServerMessageType.ERROR, "invalid game authtoken");
